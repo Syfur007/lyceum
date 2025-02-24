47065e0c4c0db5ba07e85df4b6afedf02fe29c72
Syfur007 <syfur.cse8.bu@gmail.com>
2025-02-24 06:09:04 +0600

    home ui

--- index.php
+++ index.php
@@ -7,17 +7,65 @@ if (isset($_SESSION['user_id'])) {
     exit();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lyceum Homepage</title>
+    <title>Home: Lyceum</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <h1>Welcome to Lyceum</h1>
-    <p>Please choose an option below:</p>
-    <a href="auth/login.php"><button>Login</button></a>
-    <a href="auth/register.php"><button>Register</button></a>
+
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">LYCEUM</h2>
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">SERVICE</a></li>
+                    <li><a href="#">DESIGN</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+            </div>
+
+        </div>
+        <div class="content">
+            <h1>Welcome to <br><span>Lyceum</span> <br>Connecting Minds, Empowering Learning</h1>
+            <p class="par">Lyceum is a collaborative platform that bridges the gap between students<br> and faculty, fostering seamless communication, resource sharing, and academic <br> discussions within course-based communities.</p>
+
+            <button class="cn"><a href="auth/register.php">JOIN US</a></button>
+
+            <form class="form" action="auth/login_process.php" method="POST">
+                <h2>Login Here</h2>
+                <input type="email" name="email" placeholder="Enter Email Here" required>
+                <input type="password" name="password" placeholder="Enter Password Here" required>
+                <button class="btnn" type="submit">Login</button>
+
+                <p class="link">Don't have an account<br>
+                    <a href="auth/register.php">Sign up </a> here</a>
+                </p>
+                <!-- <p class="liw">Log in with</p>
+
+                <div class="icons">
+                    <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-google"></ion-icon></a>
+                    <a href="#"><ion-icon name="logo-skype"></ion-icon></a>
+                </div> -->
+
+            </form>
+        </div>
+    </div>
+    </div>
+    </div>
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
 </body>
+
 </html>
